4e96c2fd9b93d865941f1ddb174a0dbd0e285e11
OleksiyBodnia <90392804+OleksiyBodnia@users.noreply.github.com>
2024-01-30 16:35:23 +0200

    add solution for tasks 1-2

---
+++ src/js/2-form.js
@@ -0,0 +1,29 @@
+const form = document.querySelector(".feedback-form");
+const email = form.elements.email;
+const message = form.elements.message;
+const localStorageKey = "feedback-form-state";
+
+function getFormState(){
+    const storedState = localStorage.getItem(localStorageKey);
+    return storedState ? JSON.parse(storedState) : {};
+}
+
+function saveFormState(){
+    const formState = {email: email.value.trim(), message: message.value.trim()};
+    localStorage.setItem(localStorageKey, JSON.stringify(formState));
+}
+
+const savedFormState = getFormState();
+email.value = savedFormState.email || "";
+message.value = savedFormState.message || "";
+
+form.addEventListener('input', (event) => {
+    saveFormState();
+})
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log({email: email.value.trim(), message: message.value.trim()})
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+})
